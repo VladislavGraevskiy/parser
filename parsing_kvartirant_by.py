6dffdc5739e4ef504628ad11564b9b3aeded42c3
Uladzislau <vladislavgraevskiy@gmail.com>
2016-10-21 23:24:09 +0300

    parsing: onliner with API and kvartirant.by

---
+++ parsing_kvartirant_by.py
@@ -0,0 +1,28 @@
+import utils
+
+
+def page_count(url_contents):
+    return url_contents.xpath('//div[@class="pages-nums"]/select/option[last()]/text()')
+
+
+def parse(url_contents):
+    flats = url_contents.xpath('//div[@class="adtxt_box"]')
+
+    for key, i in enumerate(flats):
+        try:
+            address = i.xpath('//span[@class="rooms"]/text()')[key]
+            rooms = i.xpath('//span[@class="rooms-box type-3"]/text()')[key]
+            price = i.xpath('//span[@class="price-box currency-840"]/text()')[key]
+            link = i.xpath('//p[@class="title"]/a/@href')[key]
+            date = i.xpath('//p[@class="date"]/text()')[key]
+            print(address.strip(), rooms, price, link, date)
+            utils.create_list(address.strip(), rooms, price, link, date)
+        except IndexError:
+            continue
+        utils.csv_file_writer(utils.LIST_OF_FLATS,'names.csv')
+
+
+def run():
+    num = int(page_count(utils.url_content("http://www.kvartirant.by/ads/flats/type/rent/"))[0])
+    for d in range(num+1):
+        parse(utils.url_content("http://www.kvartirant.by/ads/flats/type/rent/page/{}/".format(d)))
---
+++ parsing_onliner.py
@@ -0,0 +1,33 @@
+import requests
+import utils
+
+
+
+
+def page_count(json_date):
+    last_page = json_date['page']['last']
+    return last_page
+
+
+def json_parse(json_date):
+
+    for i in range(int(json_date['page']['items'])):
+        flat = json_date['apartments'][i]
+        address = flat['location']['address']
+        room = flat['rent_type'].split('_')[0]
+        price = flat['price']['amount']
+        link = flat['url']
+        date = flat['last_time_up']
+        print(address, room, price, link, date)
+        arg = [address, room, price, link, date]
+        utils.create_list(*arg)
+    utils.csv_file_writer(utils.LIST_OF_FLATS, 'onliner.csv')
+
+
+def run():
+    url = 'https://ak.api.onliner.by/search/apartments{}'
+    json_date = utils.pares(url.format('/'))
+    last_page = page_count(json_date)
+
+    for i in range(1, 3):
+        json_parse(utils.pares(url.format('?page='+str(i))))
